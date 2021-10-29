fac5fc67454b642d8bc96b2c0e5461c7064e936c
3igrik <3igrik@gmail.com>
2021-10-29 11:17:29 +0500

    feat: add layout with buttons

--- test-task-angular/src/app/app.component.ts
+++ test-task-angular/src/app/app.component.ts
@@ -1,10 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { HttpService } from "./http.service";
+import { ICompanyData } from './interfaces/ICompanyData';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.scss'],
+  providers: [HttpService]
 })
-export class AppComponent {
-  title = 'test-task-angular';
+export class AppComponent implements OnInit {
+
+  companies: ICompanyData[] = [];
+
+  constructor (private httpService: HttpService){};
+
+  ngOnInit() {
+    this.httpService.getData().subscribe((data: any) => this.companies = data)
+  }
 }

58b08172077a2fcc8863ce44fc45a288c30f388b
3igrik <3igrik@gmail.com>
2021-10-29 10:33:33 +0500

    feat: add service

---
+++ test-task-angular/src/app/http.service.ts
@@ -0,0 +1,15 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
+const SIZE = 10;
+const ENDPOINT = `https://random-data-api.com/api/company/random_company?size=${SIZE}`;
+
+@Injectable()
+export class HttpService {
+
+  constructor(private http: HttpClient){}
+
+  getData() {
+    return this.http.get(ENDPOINT);
+  }
+}

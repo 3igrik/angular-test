0158c7425d05a4ca10485839926fc57e96d4ea12
3igrik <3igrik@gmail.com>
2021-11-01 17:07:41 +0500

    feat: add yandex-map

--- test-task-angular/src/app/company-yandex-map/company-yandex-map.component.ts
+++ test-task-angular/src/app/company-yandex-map/company-yandex-map.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
 import { HttpService } from "../http.service";
+
+import ymaps from 'ymaps';
+
 @Component({
   selector: 'company-yandex-map',
   templateUrl: './company-yandex-map.component.html',
@@ -9,4 +11,15 @@ import { HttpService } from "../http.service";
 })
 
 export class CompanyYandexMapComponent{
+  constructor() {
+    ymaps
+  .load()
+  .then((maps: { Map: new (arg0: string, arg1: { center: number[]; zoom: number; }) => any; }) => {
+    const map = new maps.Map('map', {
+      center: [-8.369326, 115.166023],
+      zoom: 7
+    });
+  })
+  .catch((error: Error) => console.log('Failed to load Yandex Maps', error));
+  }
 }

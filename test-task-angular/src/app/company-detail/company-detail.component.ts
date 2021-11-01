360c4a4e8697d5fa976abacb9995bcc1df91e4a0
3igrik <3igrik@gmail.com>
2021-11-01 10:09:31 +0500

    fix: update app.component.html layout

--- test-task-angular/src/app/company-detail/company-detail.component.ts
+++ test-task-angular/src/app/company-detail/company-detail.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input } from '@angular/core';
+import { Component} from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { HttpService } from "../http.service";
 import { Subscription } from 'rxjs';

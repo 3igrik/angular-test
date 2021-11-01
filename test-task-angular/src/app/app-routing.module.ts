52eb8bd13716c6a2557ea5d2c8132047d842aad7
3igrik <3igrik@gmail.com>
2021-11-01 10:07:45 +0500

    feat: add company-yandex-map to routing

--- test-task-angular/src/app/app-routing.module.ts
+++ test-task-angular/src/app/app-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { CompanyDetailComponent } from './company-detail/company-detail.component';
 import { CompanyListComponent } from './company-list/company-list.component';
+import { CompanyYandexMapComponent } from './company-yandex-map/company-yandex-map.component';
 
 const routes: Routes = [
   {path: "", component: CompanyListComponent},
-  {path: "detail/:id", component: CompanyDetailComponent}
+  {path: "detail/:id", component: CompanyDetailComponent},
+  {path: "map", component: CompanyYandexMapComponent}
 ];
 
 @NgModule({
